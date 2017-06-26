10d60cd58955e019be238e15031930c396d3dc44
Pranav Karra <pranav@Murthys-MacBook-Air.local>
2017-06-25 21:54:17 -0400

    Removed some code from the matlab file

--- Project.m
+++ Project.m
@@ -18,18 +18,4 @@ resistor_values = [1450,0, -200,-500,0;
         0,0,0,0,1600];
 voltage = [9;9;0;0;0];
 I5 = (resistor_values\voltage)
-%% 
-resistor_values = [1450,0,-200,-500;
-    0,2000,-1000,-500;
-    -200,-1000,1450,0;
-    -500,-500,0,1100];
-voltage = [-9;9;0;0];
-I = (resistor_values\voltage)
-%% Including a 5th loop that is the outermost loop to check the above answer
-resistor_values = [1450,0,-200,-500,0;
-    0,2000,-1000,-500,0;
-    -200,-1000,1450,0,0;
-    -500,-500,0,1100,0
-    0,0,0,0,1600];
-voltage = [-9;9;0;0;0];
-I = (resistor_values\voltage)
+
